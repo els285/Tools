60bfa6754c553de206d609f11fec073c26126faf
Ethan Lewis Simpson <ethan.simpson@cern.ch>
2022-02-22 16:02:40 +0100

    adding requirements

--- pull_google_sheet.py
+++ pull_google_sheet.py
@@ -1,3 +1,14 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
 def pull_from_google_sheets(SCOPES,SAMPLE_SPREADSHEET_ID,**kwargs):
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
